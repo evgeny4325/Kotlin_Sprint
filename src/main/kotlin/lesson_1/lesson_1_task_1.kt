5f2690e386eb0f4acde69a5839fae62f3ecefcc8
evgeny4325 <4325@mail.ru>
2024-10-12 20:23:48 +0300

    KS-1-1 printing the screen of an online store

---
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_1
+
+
+fun main() {
+
+    val newOrderNumber = 75
+    val thankYou = "Спасибо за покупку!"
+
+    println("Номер вашего заказа $newOrderNumber. $thankYou")
+}
